b6742076d88cecd124aa73f50ef0034a9ef305f9
Tomiwa Obanla <obanlatomiwa@gmail.com>
2021-06-30 14:21:39 +0100

    added admin

--- django_node_migrations_test/settings.py
+++ django_node_migrations_test/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -96,7 +97,6 @@ else:
 DATABASES = {
     'default': DB_DETAILS
 }
-
 
 
 MEDIA_URL = '/media/'
--- migrations_test/admin.py
+++ migrations_test/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Account, Post, Comment
+
+
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    fields = ('id', 'username', 'email', 'first_name', 'last_name')
+    list_display = ('id', 'username', 'email', 'first_name', 'last_name')
+    readonly_fields = ('id',)
+    search_fields = ('username', 'email')
+    list_per_page = 20
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    fields = ('user', 'title', 'content')
+    list_display = ('user', 'title', 'content')
+    readonly_fields = ('id',)
+    search_fields = ('user', 'title')
+    list_per_page = 20
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    fields = ('user', 'post', 'content')
+    list_display = ('user', 'post', 'content')
+    readonly_fields = ('id',)
+    search_fields = ('user', 'post')
+    list_per_page = 20
